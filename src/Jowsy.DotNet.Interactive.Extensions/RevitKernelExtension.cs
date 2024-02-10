07422fecd4435220fd93bdc7bc9429452e343b3f
jowsy <saury.joel@gmail.com>
2024-02-10 16:32:50 +0100

    rewrite kernel command summary

--- src/Jowsy.DotNet.Interactive.Extensions/RevitKernelExtension.cs
+++ src/Jowsy.DotNet.Interactive.Extensions/RevitKernelExtension.cs
@@ -16,11 +16,13 @@ namespace Jowsy.DotNet.Interactive.Extensions
         {
             if (kernel is not CompositeKernel compositeKernel) return Task.CompletedTask;
 
-            compositeKernel
-                .AddKernelConnector(new ConnectRevitKernelCommand());
+                compositeKernel
+                    .AddKernelConnector(new ConnectRevitKernelCommand());
 
-            KernelInvocationContext.Current?.Display(
-    new HtmlString(@"<details><summary>Revit C# Kernel Added.</summary></details>"));
+                KernelInvocationContext.Current?.Display(
+              new HtmlString(@"<details><summary>Autodesk Revit Interactive Extension</summary>
+<p>This extension adds support for connecting to Autodesk Revit using the <code>#!connect revit</code> magic command. As example, to connect to revit 2024, run a cell using <code>#!connect revit --kernel-name revit24 --revit-version 2024</code>.</p>
+<p>Then begin with #!revit24 in a C#-cell to execute code in the revit context.</p></details>"), "text/html");
 
             return Task.CompletedTask;
         }
