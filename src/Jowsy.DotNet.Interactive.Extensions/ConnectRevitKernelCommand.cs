15839397b17eb625a5889107bcea7d955b728718
jowsy <saury.joel@gmail.com>
2024-02-10 16:31:23 +0100

    Set a timeout when connecting to revit kernel

--- src/Jowsy.DotNet.Interactive.Extensions/ConnectRevitKernelCommand.cs
+++ src/Jowsy.DotNet.Interactive.Extensions/ConnectRevitKernelCommand.cs
@@ -3,6 +3,7 @@ using Microsoft.DotNet.Interactive;
 using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.Connection;
 using Microsoft.DotNet.Interactive.Events;
+using Microsoft.DotNet.Interactive.Utility;
 using System;
 using System.Collections.Generic;
 using System.CommandLine;
@@ -17,10 +18,10 @@ namespace Jowsy.DotNet.Interactive.Extensions
     {
         public static string? RevitVersion;
         public Option<string> RevitVersionOption { get; } =
-    new("--revit-version", "version of revit to connect to")
-    {
-        IsRequired = true
-    };
+            new("--revit-version", "version of revit to connect to")
+            {
+                IsRequired = true
+            };
 
         public ConnectRevitKernelCommand() : base("revit", "connect to a revit kernel")
         {
@@ -33,75 +34,72 @@ namespace Jowsy.DotNet.Interactive.Extensions
 
             var pipeName = $"revit-kernel-{revitversion}-pipe";
 
-        
-
             var connector = new NamedPipeKernelConnector(pipeName!);
 
             var localName = commandLineContext.ParseResult.GetValueForOption(KernelNameOption);
 
-            var proxyKernel = await connector.CreateKernelAsync(localName!);
+            var proxyKernel = await connector.CreateKernelAsync(localName!).Timeout(new TimeSpan(0, 0, 10));
 
-
-            proxyKernel.AddMiddleware(async (command, context, next) =>
-            {
-                var submitCommand = command as SubmitCode;
-                if (submitCommand != null)
+                proxyKernel.AddMiddleware(async (command, context, next) =>
                 {
-                    RoslynCompilerService compilerService = new RoslynCompilerService(revitversion);
-
-                    var results = await compilerService.CompileRevitAddin(submitCommand.Code, 
-                                                                        true, 
-                                                                        async () =>
-                                                                        {
-                                                                            var result = await proxyKernel.SendAsync(new RequestValueInfos());
-
-                                                                            var valueInfosProduced = result.Events.Where(e => e is ValueInfosProduced)
-                                                                                                            .FirstOrDefault() as ValueInfosProduced;
-                                                                            if (valueInfosProduced == null)
-                                                                            {
-                                                                                return null;
-                                                                            }
-                                                                            return valueInfosProduced.ValueInfos.ToArray();
-                  
-                                                                        });
-
-                    if (results.AssemblyPath != null)
+                    var submitCommand = command as SubmitCode;
+                    if (submitCommand != null)
                     {
-                        await proxyKernel.SendAsync(new SendValue("assemblyPath", results.AssemblyPath, FormattedValue.CreateSingleFromObject(results.AssemblyPath)));
+                        RoslynCompilerService compilerService = new RoslynCompilerService(revitversion);
 
+                        var results = await compilerService.CompileRevitAddin(submitCommand.Code,
+                                                                            true,
+                                                                            async () =>
+                                                                            {
+                                                                                var result = await proxyKernel.SendAsync(new RequestValueInfos());
+
+                                                                                var valueInfosProduced = result.Events.Where(e => e is ValueInfosProduced)
+                                                                                                                .FirstOrDefault() as ValueInfosProduced;
+                                                                                if (valueInfosProduced == null)
+                                                                                {
+                                                                                    return null;
+                                                                                }
+                                                                                return valueInfosProduced.ValueInfos.ToArray();
+
+                                                                            });
+
+                        if (results.AssemblyPath != null)
+                        {
+                            await proxyKernel.SendAsync(new SendValue("assemblyPath", results.AssemblyPath, FormattedValue.CreateSingleFromObject(results.AssemblyPath)));
+
+                        }
+                        else
+                        {
+                            context.DisplayStandardError(results.DiagnosticText);
+                            await proxyKernel.SendAsync(new SendValue("assemblyPath", ""));
+                        }
+
+
+                        await next(command, context);
                     }
                     else
                     {
-                        context.DisplayStandardError(results.DiagnosticText);
-                        await proxyKernel.SendAsync(new SendValue("assemblyPath", ""));
+                        await next(command, context);
                     }
 
 
-                    await next(command, context);
-                }
-                else
+
+
+                });
+                proxyKernel.UseValueSharing();
+
+                //Add support for sendvalue
+                proxyKernel.KernelInfo.SupportedKernelCommands.Add(new KernelCommandInfo("SendValue"));
+                proxyKernel.KernelInfo.SupportedKernelCommands.Add(new KernelCommandInfo("RequestValueInfos"));
+
+                var reqHover = proxyKernel.KernelInfo.SupportedKernelCommands.Where(c => c.Name == "RequestHoverText").FirstOrDefault();
+                if (reqHover != null)
                 {
-                    await next(command, context);
+                    proxyKernel.KernelInfo.SupportedKernelCommands.Remove(reqHover);
                 }
+        
+                return new ProxyKernel[] { proxyKernel };
 
-
-
-
-            });
-            proxyKernel.UseValueSharing();
-
-            //Add support for sendvalue
-            proxyKernel.KernelInfo.SupportedKernelCommands.Add(new KernelCommandInfo("SendValue"));
-            proxyKernel.KernelInfo.SupportedKernelCommands.Add(new KernelCommandInfo("RequestValueInfos"));
-
-            var reqHover = proxyKernel.KernelInfo.SupportedKernelCommands.Where(c => c.Name == "RequestHoverText").FirstOrDefault();
-            if (reqHover != null)
-            {
-                proxyKernel.KernelInfo.SupportedKernelCommands.Remove(reqHover);
-            }
-
-            return new ProxyKernel[] { proxyKernel };
         }
-
     }
 }
